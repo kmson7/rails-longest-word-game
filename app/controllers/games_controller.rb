5cb3b7fb0f0470291fff5d2ef79e8863d9887afc
Keith Shepherdson <kmson7@gmail.com>
2020-11-23 16:45:11 +0800

    logic should work for checking if letters are in generated

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,7 +5,20 @@ class GamesController < ApplicationController
   end
 
   def score
-    raise
-    # @word = params['word']
+    # I probably dont have to use @ unless I am passing to the view.
+    user_letters = params['word'].split('')
+    given_letters = params['letters'].split
+
+    user_letters.each do |letter|
+      binding.pry
+      if given_letters.include?(letter)
+        given_letters.delete(letter)
+      else
+        puts "fail, no such letter in given"
+        return false
+      end
+    end
+    puts "success!!"
+    return true
   end
 end
